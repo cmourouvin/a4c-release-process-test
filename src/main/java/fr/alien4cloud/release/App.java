6468618ed5afc2dead98c8e9946a4d5549e9ef96
Cédric Mourouvin <mourouvin.cedric@gmail.com>
2015-09-10 14:05:11 +0200

    a4c release process automation with GoCD (mock project)

---
+++ src/main/java/fr/alien4cloud/release/App.java
@@ -0,0 +1,13 @@
+package fr.alien4cloud.release;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
